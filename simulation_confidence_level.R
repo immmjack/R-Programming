1878de72e28e875f2370a9280fa40f12755797e4
Jack Yang <51064693+immmjack@users.noreply.github.com>
2020-04-10 21:41:56 +0800

    add comment

--- simulation_confidence_level.R
+++ simulation_confidence_level.R
@@ -1,16 +1,16 @@
-n = 25
-c = 0.59
-mu = 7
+n = 25      # sample size
+c = 0.59    # our answer from 3a
+mu = 7      # the value for the mean
 
-trials = 50000
-capture = c(TRUE, FALSE)
+trials = 50000              # number of intervals to make
+capture = c(TRUE, FALSE)    # make a vector to hold trues and falses
 
 for (i in 1 : trials) {
     data = rnorm(25, mean = 7, sd = 4)
     xbar = mean(data)
-    LB = xbar - 1.6
-    UB = xbar + c * 0.8
-    capture[i] = (mu > LB && mu < UB)
+    LB = xbar - 1.6         # the lower bound of the current interval
+    UB = xbar + c * 0.8     # the upper bound of the current interval
+    capture[i] = (mu > LB && mu < UB)   # did we get mu?
 }
 
-print(mean(capture))
+print(mean(capture))    # calculate and display capture percentage
